53082d09110bef02cda6403c068de4f538596302
mikeder <mikedernet@gmail.com>
2020-01-30 20:44:56 -0500

    k

--- internal/blog/blog.go
+++ internal/blog/blog.go
@@ -5,16 +5,6 @@ import (
 	"database/sql"
 )
 
-// Store is where we kep blog posts and related information.
-type Store struct {
-	db *sql.DB
-}
-
-// New returns a pointer to a blog Store.
-func New(db *sql.DB) *Store {
-	return &Store{db: db}
-}
-
 // Post contains the fields for a blog post.
 type Post struct {
 	ID        int    `json:"id"`
@@ -25,6 +15,49 @@ type Post struct {
 	HTML      string `json:"html"`
 	Published string `json:"published"`
 	Updated   string `json:"updated"`
+}
+
+// Store is where we kep blog posts and related information.
+type Store struct {
+	db *sql.DB
+}
+
+// New returns a pointer to a blog Store.
+func New(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+// GetPosts returns a []Post of all the posts from the database.
+func (s *Store) GetPosts(ctx context.Context, page int) ([]Post, error) {
+	var offset int
+	if page > 0 {
+		offset = page * 5
+	}
+	results, err := s.db.QueryContext(ctx, "SELECT * FROM entries ORDER BY published DESC LIMIT ?, 5", offset)
+	if err != nil {
+		return nil, err
+	}
+	defer results.Close()
+
+	var posts []Post
+	for results.Next() {
+		var post Post
+		err = results.Scan(
+			&post.ID,
+			&post.Author,
+			&post.Slug,
+			&post.Title,
+			&post.Markdown,
+			&post.HTML,
+			&post.Published,
+			&post.Updated,
+		)
+		if err != nil {
+			return posts, err
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
 }
 
 // GetPostBySlug returns a *Post by its slug.
@@ -52,28 +85,23 @@ func (s *Store) GetPostBySlug(ctx context.Context, slug string) (*Post, error) {
 	return &post, nil
 }
 
-// GetPosts returns a []Post of all the posts from the database.
-func (s *Store) GetPosts(ctx context.Context, page int) ([]Post, error) {
-	var offset int
-	if page > 0 {
-		offset = page * 5
-	}
-	results, err := s.db.QueryContext(ctx, "SELECT * FROM entries ORDER BY published DESC LIMIT ?, 5", offset)
+// GetArchive returns a []Post of archived post data.
+func (s *Store) GetArchive(ctx context.Context) ([]Post, error) {
+	const q string = "SELECT ID, Author, Slug, Title, Published, Updated FROM entries"
+	rows, err := s.db.QueryContext(ctx, q)
 	if err != nil {
 		return nil, err
 	}
-	defer results.Close()
 
 	var posts []Post
-	for results.Next() {
+	for rows.Next() {
 		var post Post
-		err = results.Scan(
+
+		err = rows.Scan(
 			&post.ID,
 			&post.Author,
 			&post.Slug,
 			&post.Title,
-			&post.Markdown,
-			&post.HTML,
 			&post.Published,
 			&post.Updated,
 		)
