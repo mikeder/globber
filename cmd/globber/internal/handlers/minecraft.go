6aeef99380b50de84c4406e8a8c967baa8d7a6c4
mikeder <mike.eder@bethesda.net>
2020-03-11 11:24:41 -0400

    replace nulls

--- cmd/globber/internal/handlers/minecraft.go
+++ cmd/globber/internal/handlers/minecraft.go
@@ -4,16 +4,17 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/mikeder/globber/internal/minecraft"
 	"github.com/mikeder/globber/internal/web"
 )
 
 func (s *site) minecraftPing(w http.ResponseWriter, r *http.Request) {
-	ping, err := s.mc.Ping()
+	err := s.mc.Ping()
 	if err != nil {
 		web.Respond(w, struct{ error }{err}, http.StatusInternalServerError)
 	}
-	web.Respond(w, struct{ time.Duration }{ping}, http.StatusOK)
+	web.Respond(w, struct {
+		Latency time.Duration `json:"latency"`
+	}{s.mc.Latency}, http.StatusOK)
 }
 
 func (s *site) minecraftStatus(w http.ResponseWriter, r *http.Request) {
@@ -21,5 +22,5 @@ func (s *site) minecraftStatus(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		web.Respond(w, struct{ error }{err}, http.StatusInternalServerError)
 	}
-	web.Respond(w, struct{ minecraft.Server }{*s.mc}, http.StatusOK)
+	web.Respond(w, *s.mc, http.StatusOK)
 }
--- internal/minecraft/status.go
+++ internal/minecraft/status.go
@@ -1,21 +1,24 @@
 package minecraft
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"strings"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 // Server represents a Minecraft server.
 type Server struct {
-	Address        string
-	Online         bool
-	Version        string
-	MOTD           string
-	CurrentPlayers string
-	MaxPlayers     string
-	Latency        time.Duration
+	Address        string        `json:"address"`
+	Online         bool          `json:"online"`
+	Version        string        `json:"version"`
+	MOTD           string        `json:"motd"`
+	CurrentPlayers string        `json:"current_players"`
+	MaxPlayers     string        `json:"max_players"`
+	Latency        time.Duration `json:"latency"`
 }
 
 // NewServer returns a server.
@@ -28,29 +31,39 @@ func NewServer(host string, port int) *Server {
 }
 
 // Ping returns a server latency and/or connection error.
-func (s *Server) Ping() (time.Duration, error) {
+func (s *Server) Ping() error {
 	start := time.Now()
-	_, err := net.DialTimeout("tcp", s.Address, time.Duration(3)*time.Second)
+	conn, err := net.DialTimeout("tcp", s.Address, time.Duration(3)*time.Second)
+	if err != nil {
+		return errors.Wrap(err, "connecting to server")
+	}
+	defer conn.Close()
+
 	s.Latency = time.Since(start).Round(time.Millisecond)
-	return s.Latency, err
+	return nil
 }
 
 // Status updates a server status or returns connection error.
 func (s *Server) Status() error {
+	start := time.Now()
 	conn, err := net.DialTimeout("tcp", s.Address, time.Duration(3)*time.Second)
+	if err != nil {
+		return errors.Wrap(err, "connecting to server")
+	}
 	defer conn.Close()
+	s.Latency = time.Since(start).Round(time.Millisecond)
 
 	_, err = conn.Write([]byte("\xFE\x01"))
 	if err != nil {
 		s.Online = false
-		return err
+		return errors.Wrap(err, "writing to server connection")
 	}
 
 	data := make([]byte, 512)
 	_, err = conn.Read(data)
 	if err != nil {
 		s.Online = false
-		return err
+		return errors.Wrap(err, "reading from server connection")
 	}
 
 	if data == nil || len(data) == 0 {
@@ -58,16 +71,20 @@ func (s *Server) Status() error {
 		return err
 	}
 
-	parsed := strings.Split(string(data[:]), "\x00\x00\x00")
+	parsed := bytes.Split(data[:], []byte("\x00\x00\x00"))
 	if parsed != nil && len(parsed) >= 6 {
 		s.Online = true
-		s.Version = parsed[2]
-		s.MOTD = parsed[3]
-		s.CurrentPlayers = parsed[4]
-		s.MaxPlayers = parsed[5]
+		s.Version = replaceNulls(parsed[2])
+		s.MOTD = replaceNulls(parsed[3])
+		s.CurrentPlayers = replaceNulls(parsed[4])
+		s.MaxPlayers = replaceNulls(parsed[5])
 	} else {
 		s.Online = false
 	}
 
 	return nil
+}
+
+func replaceNulls(b []byte) string {
+	return strings.ReplaceAll(string(b), "\u0000", "")
 }
