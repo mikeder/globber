ebd19699b60f62fca9c267ec4d1ef06605278222
mikeder <mikedernet@gmail.com>
2020-03-15 14:13:53 -0400

    new mc page

---
+++ static/js/minecraft.js
@@ -0,0 +1,36 @@
+var getStatus = function () {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var status = JSON.parse(this.responseText);
+            updateStatus(status);
+        }
+    };
+    xhttp.open("GET", "/minecraft/status", true);
+    xhttp.send();
+}
+
+var periodicUpdate = setInterval(getStatus, 5000);
+
+var updateStatus = function (server) {
+    var banner = document.getElementById("mcstatus");
+
+    if (server.online) {
+        banner.classList.remove("alert-danger");
+        banner.classList.remove("alert-warning");
+        banner.classList.add("alert-success");
+        banner.innerText = "Server Status: ONLINE"
+    } else {
+        banner.classList.remove("alert-success");
+        banner.classList.remove("alert-warning");
+        banner.classList.add("alert-danger");
+        banner.innerText = "Server Status: OFFLINE"
+    }
+
+    document.getElementById("servername").innerHTML = server.motd;
+    document.getElementById("address").innerHTML = "<b>Address:</b> " + server.address;
+    document.getElementById("players").innerHTML = "<b>Players:</b> " + server.current_players + "/" + server.max_players;
+    document.getElementById("latency").innerHTML = "<b>Latency:</b> " + server.latency_ms + "ms";
+    document.getElementById("version").innerHTML = "<b>Version:</b> " + server.version;
+}
+
