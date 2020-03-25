2f878e193b18c0cf502c2617b5ec4650e8fbeb53
mikeder <mikedernet@gmail.com>
2020-03-24 22:52:10 -0400

    d

--- static/js/auth.js
+++ static/js/auth.js
@@ -29,15 +29,15 @@ var removeJWT = function () {
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       console.log("logout done")
+      location.replace("/blog");
     } else {
       document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;';
       document.cookie = 'jwt_refresh=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/;';
+      location.replace("/blog");
     }
   };
   xhttp.open("POST", "/auth/logout", true);
   xhttp.send();
-  
-  location.replace("/blog");
 }
 
 function getCookie(cname) {
